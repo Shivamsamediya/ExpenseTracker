20d627bd3fb0a04f4e1f475f5c8927e84d9a6467
Shivamsamediya <shivamsamediya7@gmail.com>
2025-04-28 13:13:29 +0530

    final update!!

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:[ 'http://localhost:5173' ],
+    origin:[ 'http://localhost:5173','https://expense-tracker-new-frontend.vercel.app' ],
     credentials: true,            
 }));
 
